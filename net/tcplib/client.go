328986e7ea142c73236d57b21008b0ca91ce4114
chuck1024 <chuck.ch1024@outlook.com>
2019-06-22 00:05:18 +0800

    fix tcp client

--- net/tcplib/client.go
+++ net/tcplib/client.go
@@ -189,7 +189,7 @@ func clientHandleConnection(c *Client, conn io.ReadWriteCloser) {
 	}
 
 	if err != nil {
-		logging.Error("[clientHandleConnection] occur error: ", c.Addr+", "+err.Error())
+		logging.Error("[clientHandleConnection] occur error: %s ", c.Addr+", %s"+err.Error())
 	}
 
 	for _, m := range pendingRequests {
