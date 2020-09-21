3c4fe0717459c121f04bd9b7a91eca74b2809eb2
chuck1024 <chuck.ch1024@outlook.com>
2020-09-21 17:15:16 +0800

    fix log and go mod

--- go.mod
+++ go.mod
@@ -3,30 +3,53 @@ module github.com/chuck1024/gd
 go 1.14
 
 require (
-	github.com/DATA-DOG/go-sqlmock v1.5.0 // indirect
-	github.com/Pallinder/go-randomdata v1.2.0 // indirect
+	github.com/DATA-DOG/go-sqlmock v1.5.0
+	github.com/Pallinder/go-randomdata v1.2.0
 	github.com/bitly/go-simplejson v0.5.0
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
+	github.com/coreos/bbolt v1.3.5 // indirect
 	github.com/coreos/etcd v3.3.18+incompatible // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf // indirect
 	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
-	github.com/etcd-io/etcd v3.3.18+incompatible
-	github.com/facebookgo/structtag v0.0.0-20150214074306-217e25fb9691 // indirect
-	github.com/garyburd/redigo v1.6.2 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/elazarl/goproxy v0.0.0-20200809112317-0581fc3aee2d // indirect
+	github.com/etcd-io/etcd v3.3.25+incompatible
+	github.com/facebookgo/structtag v0.0.0-20150214074306-217e25fb9691
+	github.com/garyburd/redigo v1.6.2
 	github.com/gin-gonic/gin v1.5.0
 	github.com/go-errors/errors v1.1.1
-	github.com/go-redis/redis v6.15.9+incompatible // indirect
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/google/btree v1.0.0 // indirect
 	github.com/google/uuid v1.1.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2 // indirect
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.15.0 // indirect
+	github.com/jonboulle/clockwork v0.2.1 // indirect
+	github.com/onsi/ginkgo v1.14.1 // indirect
+	github.com/onsi/gomega v1.10.2 // indirect
 	github.com/parnurzeal/gorequest v0.2.16
+	github.com/prometheus/client_golang v1.7.1 // indirect
 	github.com/samuel/go-zookeeper v0.0.0-20190923202752-2cc03de413da
 	github.com/smartystreets/goconvey v1.6.4
+	github.com/soheilhy/cmux v0.1.4 // indirect
+	github.com/tmc/grpc-websocket-proxy v0.0.0-20200427203606-3cfed13b9966 // indirect
 	github.com/v2pro/plz v0.0.0-20180227161703-2d49b86ea382
+	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
+	go.etcd.io/bbolt v1.3.5 // indirect
 	go.uber.org/zap v1.13.0 // indirect
-	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
-	google.golang.org/grpc v1.26.0 // indirect
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
 	gopkg.in/ini.v1 v1.57.0
 	moul.io/http2curl v1.0.0 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
 )
 
-replace github.com/coreos/go-systemd => github.com/coreos/go-systemd/v22 v22.0.0
+replace (
+	github.com/coreos/bbolt => go.etcd.io/bbolt v1.3.4
+	github.com/coreos/go-systemd => github.com/coreos/go-systemd/v22 v22.0.0
+	google.golang.org/grpc => google.golang.org/grpc v1.26.0
+)
--- log.go
+++ log.go
@@ -8,14 +8,11 @@ package gd
 import (
 	"encoding/xml"
 	"fmt"
+	"github.com/chuck1024/gd/utls"
 	"os"
 	"path/filepath"
 	"strings"
 	"sync"
-	"syscall"
-	"time"
-
-	"github.com/chuck1024/gd/utls"
 )
 
 var (
@@ -23,7 +20,6 @@ var (
 	logConfigFile  = "conf/log.xml"
 	defaultLogDir  = "log"
 	defaultFormat  = "%L	%D %T	%l	%I	%G	%M	%S"
-	inContainerEnv = false
 )
 
 type xmlLoggerConfig struct {
@@ -44,25 +40,18 @@ type xmlFilter struct {
 	Property []xmlProperty `xml:"property"`
 }
 
-func init() {
-	contAppName := os.Getenv("CONTAINER_S_APPNAME")
-	if contAppName != "" {
-		inContainerEnv = true
+func getInfoFileName(binName string, port int) string {
+	if port == 0 {
+		return fmt.Sprintf("%s.log", binName)
 	}
+	return fmt.Sprintf("%s_%d.log", binName, port)
 }
 
-func getInfoFileName(binname string, port int) string {
+func getWarnFileName(binName string, port int) string {
 	if port == 0 {
-		return fmt.Sprintf("%s.log", binname)
+		return fmt.Sprintf("%s_err.log", binName)
 	}
-	return fmt.Sprintf("%s_%d.log", binname, port)
-}
-
-func getWarnFileName(binname string, port int) string {
-	if port == 0 {
-		return fmt.Sprintf("%s_err.log", binname)
-	}
-	return fmt.Sprintf("%s_err_%d.log", binname, port)
+	return fmt.Sprintf("%s_err_%d.log", binName, port)
 }
 
 func RestoreLogConfig(configFilePath string, binName string, port int, logLevel string, logDir string) error {
@@ -74,41 +63,6 @@ func RestoreLogConfig(configFilePath string, binName string, port int, logLevel 
 
 	if configFilePath == "" {
 		configFilePath = logConfigFile
-	}
-
-	// redirect stdout and stderr to file
-	if inContainerEnv {
-		stdLogFileName := fmt.Sprintf("%s/%s", logDir, "stderr_process.log")
-		if utls.PathExists(stdLogFileName) {
-			//Mon Jan 2 15:04:05 -0700 MST 2006
-			ct := time.Now().Format("2006-01-02-15:04:05")
-			newPath := stdLogFileName + "-" + ct
-			err := os.Rename(stdLogFileName, newPath)
-			fmt.Println(fmt.Sprintf("rename %v => %v", stdLogFileName, newPath))
-			if err != nil {
-				fmt.Println(fmt.Sprintf("rename stderr file fail,f=%v,nf=%v,err=%v", stdLogFileName, newPath, err))
-			}
-		}
-
-		if !utls.PathExists(stdLogFileName) {
-			err := utls.EnsureDir(logDir)
-			if err != nil {
-				return fmt.Errorf("ensure dir fail,dir=%v,err=%w", logDir, err)
-			}
-			err = utls.Store2File(stdLogFileName, "")
-			if err != nil {
-				return fmt.Errorf("create file fail,name=%v,err=%w", stdLogFileName, err)
-			}
-		}
-
-		stdLogFile, err := os.OpenFile(stdLogFileName, os.O_WRONLY|os.O_CREATE|os.O_SYNC, 0755)
-		if err != nil {
-			return fmt.Errorf("open std log file fail,file=%v,err=%w", stdLogFileName, err)
-		}
-		err = syscall.Dup2(int(stdLogFile.Fd()), 2)
-		if err != nil {
-			return fmt.Errorf("redirect stderr to file fail,file=%v,err=%w", stdLogFileName, err)
-		}
 	}
 
 	if logLevel == "" {
@@ -127,7 +81,7 @@ func RestoreLogConfig(configFilePath string, binName string, port int, logLevel 
 		binName = ex
 	}
 
-	if logLevel != "DEBUG" && logLevel != "INFO" && logLevel != "WARNING" && logLevel != "ErrOR" {
+	if logLevel != "DEBUG" && logLevel != "INFO" && logLevel != "WARNING" && logLevel != "ERROR" {
 		return fmt.Errorf("invalid log level %v", logLevel)
 	}
 
