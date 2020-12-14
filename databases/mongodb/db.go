899f25563504b95ce3af93549750286b87cceb01
chuck1024 <chuck.ch1024@outlook.com>
2020-12-14 17:36:20 +0800

    add mongo gl and pc

--- databases/mongodb/db.go
+++ databases/mongodb/db.go
@@ -21,14 +21,6 @@ import (
 )
 
 const (
-	DefaultMaxActive    = 500
-	DefaultMaxIdle      = 8
-	DefaultIdleTimeout  = 300
-	DefaultRetryTimes   = 3
-	DefaultConnTimeout  = 400
-	DefaultReadTimeout  = 700
-	DefaultWriteTimeout = 500
-
 	MongoCommonCostMax   = 20
 	MongoCmd             = "mongo_cmd_%v"
 	MongoCmdSlowCount    = "mongo_%v_slow_count"
