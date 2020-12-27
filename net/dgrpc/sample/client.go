6c26509df73a1b2071a1fb23ddf52ad2dd61f616
chuck1024 <chuck.ch1024@outlook.com>
2020-12-27 17:25:00 +0800

    fix grpc sample client

--- net/dgrpc/sample/client.go
+++ net/dgrpc/sample/client.go
@@ -25,7 +25,6 @@ func (s *server2) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloR
 }
 
 func main() {
-	var i chan struct{}
 	bc := dgrpc.GrpcClient{
 		Target:      "127.0.0.1:10240",
 		ServiceName: "gd",
@@ -52,5 +51,4 @@ func main() {
 
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
 	dlog.Debug(fmt.Sprintf("Greeting: %s, err=%v", r, err))
-	<-i
 }
